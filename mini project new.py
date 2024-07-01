8eec13b3eeff0accf549d589bd5ba6270a330f96
msiddiqu <msiddique0327bh@gmail.com>
2024-07-01 11:03:42 +0500

    update_inventory

--- mini project new.py
+++ mini project new.py
@@ -34,3 +34,15 @@ def change_price():
           else:
                print("Item not available")
                exit()
+
+def update_inventory():
+     name=input("Enter the name of the item:")
+     count=int(input("Enter the new count of the item:"))
+     for key , values in inventory.items():
+          if key==name:
+               inventory[key]["count"]=count
+               print("Count updated successfully")
+               break
+          else:
+               print("item not available")
+               exit()
